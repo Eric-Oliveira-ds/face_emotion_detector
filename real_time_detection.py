497b85131350e26d91f2617403d6e54e27b3f870
Eric <eric.santoss@fbter.org.br>
2024-02-09 20:33:11 -0300

    Add perfect test for send message with pywhatkit

--- real_time_detection.py
+++ real_time_detection.py
@@ -1,6 +1,19 @@
 import cv2
 from keras.models import model_from_json
 import numpy as np
+import pywhatkit as kit
+from datetime import datetime
+import time
+import yaml
+
+
+with open("config.yaml", "r") as config_file:
+    config_data = yaml.safe_load(config_file)
+
+phone_number = config_data.get("phone_number", "")
+paciente = config_data.get("paciente", "")
+message = None
+
 # from keras_preprocessing.image import load_img
 json_file = open("emotion_detector.json", "r")
 model_json = json_file.read()
@@ -23,7 +36,13 @@ webcam = cv2.VideoCapture(0)
 #           3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
 labels = {0: 'Nervoso(a)', 1: 'Nojo', 2: 'Medo',
           3: 'Feliz', 4: 'Neutro', 5: 'Bravo(a)', 6: 'Surpreso(a)'}
+
 while True:
+
+    agora = datetime.now()
+    hora_atual = agora.hour
+    minuto_atual = agora.minute + 1
+
     i, im = webcam.read()
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(im, 1.3, 5)
@@ -35,10 +54,27 @@ while True:
             img = extract_features(image)
             pred = model.predict(img)
             prediction_label = labels[pred.argmax()]
-            # print("Predicted Output:", prediction_label)
+
+            if prediction_label == 0:
+                # Specify the phone number including the country code and the message
+                message = f"Olá, o paciente {paciente}, está nervoso!"
+            elif prediction_label == 2:
+                # Specify the phone number including the country code and the message
+                message = f"Olá, o paciente {paciente}, está com medo!"
+            elif prediction_label == 6:
+                # Specify the phone number including the country code and the message
+                message = f"Olá, o paciente {paciente}, está surpreso!"
+            elif prediction_label == 3:
+                # Specify the phone number including the country code and the message
+                message = f"Olá, o paciente {paciente}, está feliz!"
+            else:
+                message = f"Olá, o paciente {paciente}, está {prediction_label}!"
+
+            kit.sendwhatmsg(phone_number, message, hora_atual, minuto_atual, tab_close=True, close_time=2)
+            time.sleep(3)
+
             # cv2.putText(im, prediction_label)
-            cv2.putText(im, '% s' % (prediction_label), (p-10, q-10),
-                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
+            cv2.putText(im, '% s' % (prediction_label), (p-10, q-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
         cv2.imshow("Output", im)
         cv2.waitKey(27)
     except cv2.error:
